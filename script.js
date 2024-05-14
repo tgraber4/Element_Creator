f35e0c0e5166752bd4483d291d8fc3a24a6075c4
tgraber4 <tgraber4@gmail.com>
2024-05-14 18:38:58 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* <h2> 2023 - 2024 </h2>
+        <div class="flexbox">
+            <div class="team">
+                <p class="title">President</p>
+                <p>Graham Bond</p>
+            </div>
+            <div class="team">
+                <p class="title">Vice President</p>
+                <p>Luke Sanders</p>
+            </div>
+            <div class="team">
+                <p class="title">Team Leads</p>
+                <p>Logan Frommelt</p>
+                <p>Michael Mischkot</p>
+                <p>Landon Toler</p>
+            </div>
+        </div>
+        <div class="gap"></div>
+        <div class="flexbox">
+            <div class="team">
+                <p class="title2">Design Proposal</p>
+                <p>2nd / 149</p>
+            </div>
+            <div class="team">
+                <p class="title2">Design Report</p>
+                <p>TBD</p>
+            </div>
+            <div class="team">
+                <p class="title2">Fly-Off</p>
+                <p>TBD</p>
+            </div>
+        </div>
+        <div class="gap"></div>
+        <h3 class="update_title">Updates</h3>
+        <h3> Mizzou AeroTigers Co. founded as a 501(c)(3) nonprofit corporation</h3>
+        <div class="gap"></div> */
